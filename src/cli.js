b676184ae717bf63404980c4e120048ff35742c6
Александр Шишкин <shishkin_aleksandr@mail.ru>
2024-09-20 14:26:23 +0300

    update src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export function yourName() {
-const name = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + name + '!');
-};
+  const name = readlineSync.question('May I have your name? ');
+  console.log('Hello, ' + name + '!');
+}
