f34a4670845bd9ac06ffecc895d58ba5cb33011d
Александр <shishkin_aleksandr@mail.ru>
2024-09-25 21:27:32 +0300

    update src/even.js

--- src/even.js
+++ src/even.js
@@ -37,5 +37,43 @@ export default () => {
   Let's try again, ${name}!`);
   }
 
-  return console.log('It WORK!');
+  const secondNumber = getRandomIntInclusive(0, 100);
+  const residueSecondNumber = secondNumber % 2;
+
+  if (residueSecondNumber === 0) {
+    parityFirstNumber = 'yes';
+  } else {
+    parityFirstNumber = 'no';
+  }
+
+  console.log(`Question: ${secondNumber}`);
+  const secondResponse = readlineSync.question('Your answer: ');
+
+  if (secondResponse === parityFirstNumber) {
+    console.log('Correct!');
+  } else {
+    return console.log(`${secondResponse} is wrong answer ;(. Correct answer was ${parityFirstNumber}.
+  Let's try again, ${name}!`);
+  }
+
+  const thirdNumber = getRandomIntInclusive(0, 100);
+  const residueThirdNumber = thirdNumber % 2;
+
+  if (residueThirdNumber === 0) {
+    parityFirstNumber = 'yes';
+  } else {
+    parityFirstNumber = 'no';
+  }
+
+  console.log(`Question: ${thirdNumber}`);
+  const thirdResponse = readlineSync.question('Your answer: ');
+
+  if (thirdResponse === parityFirstNumber) {
+    console.log('Correct!');
+  } else {
+    return console.log(`${thirdResponse} is wrong answer ;(. Correct answer was ${parityFirstNumber}.
+  Let's try again, ${name}!`);
+  }
+
+  return console.log(`Congratulations, ${name}`);
 };
