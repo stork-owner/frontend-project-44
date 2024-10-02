dc625bfbb29dc208db58001621419dee224003e1
Александр <shishkin_aleksandr@mail.ru>
2024-10-02 21:53:48 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
-const replyToUser = (a, b, c) => {
-  let result = '';
-
-  if (a === b) {
-    result = 'Correct!';
-  } else {
-    result = `${a} is wrong answer ;(. Correct answer was ${b}.
-  Let's try again, ${c}!`;
-  }
-  return console.log(result);
-};
-
-const replyToUser2 = (a, b) => {
+const replyToUser = (a, b) => {
   if (a === b) {
     return true;
   }
   return false;
 };
 
-export { replyToUser, replyToUser2 };
+export { replyToUser };
