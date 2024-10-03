1878cf327543c2b312c6c70be8d31adf21c7189a
Александр Шишкин <shishkin_aleksandr@mail.ru>
2024-10-03 12:51:16 +0300

    update gcd.js

--- src/gcd.js
+++ src/gcd.js
@@ -1,3 +1,12 @@
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
 export default () => {
-  console.log('Work');
+  // спрашиваем имя игрока
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+
+  // условие игры
+  console.log('Find the greatest common divisor of given numbers.');
 };
